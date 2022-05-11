de06fd1e5e73c0c2d5de2316d5e350fd214780c3
tw0517tw <1003146+tw0517tw@users.noreply.github.com>
2022-05-11 16:30:20 +0800

    feat: install and setup styled-components and react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import styled, { createGlobalStyle } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { useState } from "react";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const GlobalContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+`;
+
+function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <>
+      <GlobalStyle />
+
+      <QueryClientProvider client={queryClient}>
+        <GlobalContainer>
+          <Component {...pageProps} />
+        </GlobalContainer>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    // https://github.com/vercel/next.js/tree/main/examples/with-styled-components
+    // https://github.com/vercel/next.js/tree/main/examples/with-typescript-styled-components
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
